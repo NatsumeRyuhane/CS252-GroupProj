9dcf2744347c6331300712befb3fcc0432bdabee
Rona He <57215192+Rona-dt@users.noreply.github.com>
2022-04-11 12:55:06 +0800

    MGAF-19 [chore] set up the database with MySQL

---
+++ documentation/MGAF_db.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE `MGAF_db`;
+  USE `MGAF_db`;
+
+
+-- create
+CREATE TABLE users (
+  user_id     INT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_name   VARCHAR(20) NOT NULL,
+  user_password   VARCHAR(255) NOT NULL,
+  user_email  VARCHAR(255) NOT NULL,
+  user_date   DATETIME NOT NULL,
+  PRIMARY KEY (user_id)
+) ENGINE=INNODB;
+
+CREATE TABLE posts (
+  post_id         INT NOT NULL AUTO_INCREMENT,
+  post_content    TEXT NOT NULL,
+  post_date       DATETIME NOT NULL,
+  post_topic      INT(8) NOT NULL,
+  post_by     INT(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (post_id)
+) ENGINE=INNODB;
+
+ALTER TABLE posts ADD FOREIGN KEY(post_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+
+-- fetch 
+SELECT * FROM users;
