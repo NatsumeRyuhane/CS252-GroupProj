93ad76b57d6f2270679f96cc586639feab128da8
Ryuhane <natsumeryuhane@gmail.com>
2022-04-15 10:33:57 +0800

    [docs] add instructions of implementation of each unimplemented function

--- app/app.py
+++ app/app.py
@@ -12,6 +12,7 @@ app = flask.Flask(__name__)
 @app.route('/index')
 def index():
     sid = flask.request.args.get("sessionid")
+
     if sid is None:
         return flask.render_template("index.html")
     else:
@@ -34,6 +35,10 @@ def login():
 
 @app.route('/register')
 def register():
+    # TODO: implement this function
+    # 1. get the form data
+    # 2. use templated mysql command to add user to user db
+    # 3. automatically get the session id and redirect user to index page in logged in state
     pass
 
 @app.route('/user-login-auth')
@@ -45,16 +50,36 @@ def user_auth():
     if auth_user_uid:
         usr = user.User(auth_user_uid, db_conn)
         if password == usr.get_password():
-            return flask.redirect(location = f"/index?sessionid={usr.get_session_id()}", code = 302)
+            return flask.redirect(location = f"/index?sessionid={usr.set_new_session_id()}", code = 302)
         else:
             return flask.redirect(location = f"/login?loginfail=True", code = 302)
     else:
         return flask.redirect(location = f"/login?loginfail=True", code = 302)
 
-@app.route('/create-post/<post_param>')
+@app.route('/create-post')
 def create_post(post_param):
+    # TODO: implement this function
+    # 1. TODO: get user session ID from cookies <- but how do we append user seeeion id automatically to the data?
+    # 1.5 maybe instead of using default form action, we can use js code instead to implement this function?
+    # 2. pack the post-title and post-content as a json object
+    # 3. add the new post to posts database
+    # 4. redirect user to inedx.html (page refresh
     pass
 
+@app.route('/get-post/<page>')
+def get_post(page):
+    # TODO: implement this function
+    # 1. get the latest posts by call posts.get_latest_posts(page*10) <- lets say we make 10 posts a 'page'
+    # 2. parse the return value from the function
+    # 3. use jinja2 to generate HTML for each post
+    # 4. use js to update the on-screen html content
+    # the template:
+    # <div class="user-post">
+    # <div class="post-user-info">{{ post_author }}</div>
+    # <div class="post-content">{{ post_content }}</div>
+    # <div class="post-like-count">{{ post_like_count }} people liked this.</div>
+    # </div>
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
